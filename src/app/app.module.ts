3f21e73b3df6833e8269597114ed7f1f91fc9c80
Bar Ben Ezra <bar.ben-ezra@cellebrite.com>
2021-07-20 20:19:19 +0300

    initialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './assignment/task.component';
+import { EditTaskComponent } from './taskEditMode/editTask.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    EditTaskComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/taskEditMode/editTask.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'editTask',
+  templateUrl: './editTask.component.html',
+  styleUrls: ['./editTask.component.css']
+})
+export class EditTaskComponent {
+}
