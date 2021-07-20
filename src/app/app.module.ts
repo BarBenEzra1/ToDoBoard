dd85ac818d5a9ac9e8b3059d747fff688d94b1f8
Bar Ben Ezra <bar.ben-ezra@cellebrite.com>
2021-07-20 21:00:01 +0300

    add header and initial state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TaskComponent } from './assignment/task.component';
+import { TaskComponent } from './task/task.component';
 import { EditTaskComponent } from './taskEditMode/editTask.component';
 import { AlertComponent } from './alert/alert.component';
+import { HeaderComponent } from './header/header.component';
+import { TasksListComponent } from './tasksList/tasksList.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskComponent,
     EditTaskComponent,
-    AlertComponent
+    AlertComponent,
+    HeaderComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tasksList/tasksList.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tasksList',
+  templateUrl: './tasksList.component.html',
+  styleUrls: ['./tasksList.component.css']
+})
+export class TasksListComponent {
+}
